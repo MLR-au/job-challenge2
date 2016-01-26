ba71a4bd06d1bc12b4766dc456f94b3c364dbf6a
Marco La Rosa <m@lr.id.au>
2016-01-26 13:00:51 +1100

    first cut of input-processor

--- app/scripts/directives/input-processor.js
+++ app/scripts/directives/input-processor.js
@@ -7,15 +7,28 @@
  * # inputProcessor
  */
 angular.module('appApp')
-  .directive('inputProcessor', function () {
+  .directive('inputProcessor', [ '_', function (_) {
     return {
       templateUrl: 'views/directives/input-processor.html',
       restrict: 'E',
       scope: {
           inputs: '=',
-          invalid: '='
+          errors: '='
       },
       link: function postLink(scope, element, attrs) {
+          scope.validateInputs = function() {
+              scope.inputs = [];
+              scope.errors = [];
+              _.each(scope.rawInput.split('\n'), function(input) {
+                  var fields = input.split(',')
+                  if (fields.length == 5) {
+                      scope.inputs.push(input);
+                  } else {
+                      scope.errors.push(input);
+                  }
+              });
+          };
+
       }
     };
-  });
+  }]);
