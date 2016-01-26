ebbc7915ffbca8d11db876b9eccf7e82095b55e1
Marco La Rosa <m@lr.id.au>
2016-01-26 14:27:49 +1100

    add in link to stackoverflow config answer

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,6 +2,7 @@
 // http://karma-runner.github.io/0.12/config/configuration-file.html
 // Generated on 2016-01-25 using
 // generator-karma 1.0.1
+// see also: http://stackoverflow.com/a/18368287
 
 module.exports = function(config) {
   'use strict';
