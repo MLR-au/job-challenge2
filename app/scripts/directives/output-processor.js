41513ac15aacc2e99688ce37671cbac29ada1248
Marco La Rosa <m@lr.id.au>
2016-01-28 19:24:12 +1100

    flesh out the output processor and tests

--- app/scripts/directives/output-processor.js
+++ app/scripts/directives/output-processor.js
@@ -7,7 +7,7 @@
  * # outputProcessor
  */
 angular.module('appApp')
-  .directive('outputProcessor', function () {
+  .directive('outputProcessor', [ 'calculator', '_', function (calc, _) {
     return {
       templateUrl: 'views/directives/output-processor.html',
       restrict: 'E',
@@ -15,6 +15,24 @@ angular.module('appApp')
           inputs : '='
       },
       link: function postLink(scope, element, attrs) {
+          scope.$watch('inputs', function() {
+              scope.createOutput();
+          }, true);
+
+          scope.createOutput = function() {
+              scope.output = _.map(scope.inputs, function(input) {
+                  var monthlyGrossIncome = calc.calculateMonthlyGrossIncome(input.salary);
+                  var tax = calc.calculateTax(input.salary);
+                  return {
+                      'name': input.fullName,
+                      'period': input.payPeriod,
+                      'grossIncome': monthlyGrossIncome,
+                      'tax': tax,
+                      'netIncome': calc.calculateNetIncome(monthlyGrossIncome, tax),
+                      'super': calc.calculateSuper(monthlyGrossIncome, input.superRate)
+                  }
+              });
+          };
       }
     };
-  });
+  }]);
--- test/spec/directives/output-processor.js
+++ test/spec/directives/output-processor.js
@@ -12,10 +12,26 @@ describe('Directive: outputProcessor', function () {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
         scope = $rootScope.$new();
-        element = angular.element('<output-processor inputs="inputs" errors="errors"></output-processor>');
+        element = angular.element('<output-processor inputs="inputs"></output-processor>');
         element = $compile(element)(scope);
         scope.$digest();
         scope = element.isolateScope();
+
+        scope.inputs = [
+            { 'fullName': 'David Rudd', 'payPeriod': '01 March – 31 March', 'salary': 60050, 'superRate': 9 }, 
+            { 'fullName': 'Ryan Chen', 'payPeriod': '01 March – 31 March', 'salary': 120000, 'superRate': 10}
+        ];
+        scope.createOutput();
     }));
+
+    it('should have a method to create the output data structure', function() {
+        expect(scope.createOutput).toBeDefined();
+    });
+    it('should create an output scope variable', function() {
+        expect(scope.output).toBeDefined();
+    });
+    it('should create an output scope variable with 2 elements', function() {
+        expect(scope.output.length).toBe(2);
+    });
 
 });
